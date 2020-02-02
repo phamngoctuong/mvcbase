ef31bdb1df1213180fa9d3dae3e577bd46194b36
Lionel <phamngoctuong1805@gmail.com>
2020-02-02 12:21:46 +0700

    explode url to array, detail views Screenshot_5.3.png, Screenshot_5.4.png

--- app/core/App.php
+++ app/core/App.php
@@ -10,7 +10,9 @@
 			echo '</pre>';
 		}
 		public function parseUrl() {
-			return $_GET['url'];
+			if(isset($_GET['url'])) {
+				return $url = explode('/', filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
+			}
 		}
 	}
 ?>
